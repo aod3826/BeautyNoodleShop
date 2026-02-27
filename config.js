3fd119d1449648b6274e336eb707b840fbde624f
aod3826 <pantipa3826@gmail.com>
2026-02-28 04:37:59 +0800

    Delete config.jsร

--- config.js
+++
@@ -1,4 +0,0 @@
-// Configuration file for the web application
-const CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzHRT_TogzIchfNcdJ2MiAmtzxJJuFNQddJ6vsd2TW0pUWPx-fQyfb8MVeJ0PakpRqa/exec'
-};
